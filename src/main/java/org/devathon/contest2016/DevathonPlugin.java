84583835b39a796f89a0ce04dea9bdf8e87873d0
BuildTools <poutian.dev@gmail.com>
2016-11-05 11:36:53 -0600

    Added basic config things

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -3,10 +3,21 @@ package org.devathon.contest2016;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class DevathonPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if(!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+
+        File config = new File(getDataFolder(), "config.yml");
+
+        if(!config.exists()) {
+            saveDefaultConfig();
+        }
         startup();
     }
 
@@ -21,5 +32,7 @@ public class DevathonPlugin extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
         this.getCommand("deconstruct").setExecutor(new DeconstructCommand(this));
     }
+
+
 }
 
