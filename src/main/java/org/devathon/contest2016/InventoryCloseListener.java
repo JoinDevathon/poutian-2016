2a5fe973c95e909acc1a7d12330c90a57b3538d0
BuildTools <poutian.dev@gmail.com>
2016-11-05 17:55:50 -0600

    added salvaging

--- src/main/java/org/devathon/contest2016/InventoryCloseListener.java
+++ src/main/java/org/devathon/contest2016/InventoryCloseListener.java
@@ -1,5 +1,7 @@
 package org.devathon.contest2016;
 
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +11,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Set;
 
 public class InventoryCloseListener implements Listener {
@@ -32,14 +35,41 @@ public class InventoryCloseListener implements Listener {
             for(int i = 0; i < 9; i++) {
                 if(inv.getItem(i) != null) {
                     ItemStack is = inv.getItem(i);
-                    p.sendMessage(is.getType().toString());
 
                     if(deconItems.contains(is.getType().toString().toLowerCase())) {
-                        p.sendMessage("Contains " + is.getType().toString());
+                        Set<String> itemsToGive = plugin.getConfig().getConfigurationSection("deconstruct." + is.getType().toString().toLowerCase()).getKeys(false);
+                        for(Iterator<String> it = itemsToGive.iterator(); it.hasNext();) {
+                            String label = it.next();
+                            Material mat = Material.getMaterial(label.toUpperCase());
+                            int amount = plugin.getConfig().getInt("deconstruct." + is.getType().toString().toLowerCase() + "." + label);
+
+                            addToInv(new ItemStack(mat, amount * is.getAmount()), p);
+                            p.sendMessage(ChatColor.GREEN + "Salvaged " + is.getAmount() + " " + is.getType().toString() + " to get " + amount * is.getAmount() + " " + mat.toString());
+                        }
+                    } else {
+                        addToInv(is, p);
+                        p.sendMessage(ChatColor.RED + "You can not salvage " + is.getType().toString());
                     }
                 }
 
             }
+        }
+    }
+
+    private void addToInv(ItemStack is, Player p) {
+        Inventory inv = p.getInventory();
+        boolean full = true;
+        for(int i = 0; i < 36; i++) {
+            if(inv.getItem(i) == null) {
+                full = false;
+            }
+        }
+
+        if(!full) {
+            inv.addItem(is);
+        } else {
+            p.sendMessage(ChatColor.RED + "Your inventory is full, dropping items on ground");
+            p.getWorld().dropItem(p.getLocation(), is);
         }
     }
 }
